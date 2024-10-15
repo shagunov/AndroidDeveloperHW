0ddd8eb201a9c20baee0a8c21d43ff415625bb84
Евгний <shagunov2012.bob@gmail.com>
2024-10-15 16:50:51 +0300

    EncapsulationGetterSetter HW complete

---
+++ src/EncapsulationGetterSetter/EncapsulationGetterSetter.java
@@ -0,0 +1,29 @@
+package EncapsulationGetterSetter;
+
+public class EncapsulationGetterSetter {
+    public static void main(String[] args){
+
+        // TODO решение 2-го задания
+        Gamer[] gamers = new Gamer[]{
+                new Gamer("Anna", false),
+                new Gamer("Vlad", true),
+                new Gamer("Kyrill", false),
+                new Gamer("Kristian", true),
+                new Gamer("Marina", false),
+                new Gamer("Sergey", true)
+        };
+
+        for(Gamer gamer : gamers){
+            System.out.println(gamer);
+        }
+
+        System.out.print("\nАктивные игроки: [");
+        for (Gamer gamer : gamers){
+            if(gamer.isActive()){
+                System.out.print(gamer.getNickname() + ", ");
+            }
+        }
+        System.out.println("\b\b]");
+
+    }
+}
---
+++ src/EncapsulationGetterSetter/Gamer.java
@@ -0,0 +1,33 @@
+package EncapsulationGetterSetter;
+
+public class Gamer {
+    // TODO решение 1-го задания. Создание класса игрока
+    private String nickname;
+    private boolean isActive;
+
+    @Override
+    public String toString() {
+        return "Игрок: " + nickname + " " + (isActive() ? "активен" : "не активен");
+    }
+
+    Gamer(String nickname, boolean isActive){
+        this.nickname = nickname;
+        this.isActive = isActive;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public boolean isActive() {
+        return isActive;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public void setActive(boolean active) {
+        isActive = active;
+    }
+}
