9bc63d4e3593105183b727cd7e133ae1913f1cac
Shagunov Eugene <shagunov2012.bob@gmail.com>
2024-10-24 12:20:50 +0300

    Exception HW

---
+++ src/Exception_/Exception_.java
@@ -0,0 +1,46 @@
+package Exception_;
+
+import java.util.Scanner;
+
+public class Exception_ {
+    public static void main(String[] args){
+        try {
+            checkStroke("afda 21421 13 werf");
+        }
+        catch (Exception ex){
+            System.out.println(ex.getMessage());
+        }
+        try {
+            checkNumber(3);
+        }
+        catch (Exception ex){
+            System.out.println(ex.getMessage());
+        }
+        try {
+            checkNumber(1234341234);
+        } catch (ArithmeticException e) {
+            System.out.println(e.getMessage());
+        }
+
+        try {
+            checkNumber(12351247);
+        }
+        catch (Exception ex){
+            System.out.println(ex.getMessage());
+        }
+    }
+
+    public static void checkNumber(int number) throws ArithmeticException{
+        if(number % 2 == 1){
+            throw new ArithmeticException(number + " - нечётное число");
+        }
+    }
+
+    public static void checkStroke(String stroke) throws Exception{
+        for(int i = 0; i < stroke.length(); ++i){
+            if(stroke.charAt(i) >= '0' || stroke.charAt(i) <= '9'){
+                throw new Exception("Найдено число в строке");
+            }
+        }
+    }
+}
