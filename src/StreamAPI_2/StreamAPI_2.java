a88661d273cfdda60d8d42b5bb3ca982127253fc
Shagunov Eugene <shagunov2012.bob@gmail.com>
2024-10-30 17:40:36 +0300

    StreamAPI 2 HW complete

--- src/StreamAPI_2/StreamAPI_2.java
+++ src/StreamAPI_2/StreamAPI_2.java
@@ -3,7 +3,6 @@ package StreamAPI_2;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class StreamAPI_2 {
     public static void main(String[] args) {
