42c30ad3a2eb43ce81389f4fa55ad5e470347d81
Shagunov Eugene <shagunov2012.bob@gmail.com>
2024-11-02 11:26:36 +0300

    Added Module0 Dyrectory with project modules

---
+++ Module0/0.2.VariablesDataTypesOperators/src/Main.java
@@ -0,0 +1,25 @@
+public class Main {
+    public static void main(String[] args) {
+        int x = 30;
+        double y = 43.6;
+        char z = 't';
+
+        double result = x + y + z;
+        System.out.println(result);
+
+        int a = 5;
+        int b = 12;
+
+        boolean check = a == b;
+        System.out.println(check);
+
+        int c = 6;
+        c += 6;
+        c -= 2;
+        c /= 5;
+        c *= 7;
+
+        System.out.println( c );
+
+    }
+}
