0e1d13375632ffa2b023b9b70abf1b022ae8787d
Shagunov Eugene <shagunov2012.bob@gmail.com>
2024-11-02 21:43:40 +0300

    File HW rework

--- Module2/2.6.File/src/Main.java
+++ Module2/2.6.File/src/Main.java
@@ -1,13 +1,14 @@
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.Scanner;
+import java.util.*;
 import java.io.File;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         Scanner scanner = new Scanner(System.in);
-        File file = new File("./out/production/HelloApp/Main/data.txt");
-        FileWriter writer = new FileWriter(file, true);
+        File file = createFileIfNotExist("./out/production/2.6.File/files/data.txt");
+        System.out.println(" ");
+        FileWriter writer = new FileWriter(file);
         String stroke;
         System.out.println("Вводите данные с консоли");
         while(true){
@@ -22,5 +23,27 @@ public class Main {
         writer.flush();
         writer.close();
         scanner.close();
+    }
+
+    public static File createFileIfNotExist(String filePath) throws IOException{
+        File file = new File(filePath);
+
+        if (!file.exists()){
+            var fileName = file.getName();
+            System.out.println(fileName);
+            var filePathStrings = new Stack<String>();
+            File dirFile = null;
+
+            for(dirFile = file.getParentFile(); !dirFile.exists(); dirFile = dirFile.getParentFile()) {
+                filePathStrings.push(dirFile.getName());
+            } File file1 = dirFile;
+            while(!filePathStrings.empty()){
+                file1 = new File(file1, filePathStrings.pop());
+                if(!file1.mkdir()) throw new IOException("Cannot create dir");
+            }
+            file = new File(file1, fileName);
+        }
+
+        return file;
     }
 }
