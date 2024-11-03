c946904756ebfac1efdc50b369cbc022e6109102
Shagunov Eugene <shagunov2012.bob@gmail.com>
2024-11-03 12:29:26 +0300

    Variable HW Complete

---
+++ Module3/3.1.Variables/src/Main.kt
@@ -0,0 +1,9 @@
+
+fun main(){
+    val age = 25
+    val name = "Евгений"
+    val isStudent = true
+    val salary = 20000.0
+
+    println("Age: " + age + " | Name: " + name + " | Is Student: " + isStudent + " | Salary: " + salary)
+}
