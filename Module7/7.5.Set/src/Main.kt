a04331b2e9f7503e39a4a60b4ca0bf2be4fef7bb
Eugene Shagunov <shagunov2012.bob@gmail.com>
2024-12-17 14:33:37 +0300

    Set HW has been completed

---
+++ Module7/7.5.Set/src/Main.kt
@@ -0,0 +1,41 @@
+import kotlin.math.absoluteValue
+
+fun main() {
+    val intArray = arrayOf(1, 3, 5, 3, 123, 3, 12, 22, 1, 4, 4, 3, 4).toList()
+    val intSet = mutableSetOf<Int>().apply{ for(counter in 0 .. 19) add((-100..100).random()) }
+    val evenSet = intSet.filter { element -> element.absoluteValue % 2 == 0 }
+    val oddSet = intSet.filter { element -> element.absoluteValue % 2 == 1 }
+
+    println("Множество чисел: $intSet")
+    println("Чётные числа:    $evenSet")
+    println("Нечётные числа:  $oddSet")
+
+    val intSet2 = intSet.map { element -> element * 2 }.filter { element -> element.absoluteValue % 5 == 0 }
+    println("Тот же список, но с удвоенными элементами, кратными пяти: $intSet2")
+
+    println("Список с удалёнными повторами: ${removeDuplicates(intArray)}")
+
+    val array = arrayOf(0, 6, 0, 0, 2, 0, 0, 0, 1, 0, 5, 0)
+    println(array.contentToString())
+    println(nullsInEnd(array).contentToString())
+}
+
+fun <T> removeDuplicates(collection: Collection<T>?): Collection<T>?{
+    return collection?.toMutableSet()
+}
+
+fun nullsInEnd(array: Array<Int>): Array<Int>{
+    mainLoop@for(elemIdx in array.indices){
+        if(array[elemIdx] == 0){
+            for(elemIdx1 in elemIdx..<array.size) {
+                if (array[elemIdx1] != 0){
+                    val tmp = array[elemIdx1]
+                    array[elemIdx1] = array[elemIdx]
+                    array[elemIdx] = tmp
+                    continue@mainLoop
+                }
+            }
+        }
+    }
+    return array
+}
