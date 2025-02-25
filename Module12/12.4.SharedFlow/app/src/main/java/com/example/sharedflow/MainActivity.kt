4be9ec27e583b1e5e2d760e849ede95591d07f70
Eugen <72526004+shagunov@users.noreply.github.com>
2025-02-25 16:08:04 +0300

    Shared Flow

--- Module12/12.4.SharedFlow/app/src/main/java/com/example/sharedflow/MainActivity.kt
+++ Module12/12.4.SharedFlow/app/src/main/java/com/example/sharedflow/MainActivity.kt
@@ -1,23 +1,22 @@
-package com.example.stateflow
+package com.example.sharedflow
 
 import android.os.Bundle
 import android.util.Log
-import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.lifecycle.lifecycleScope
 import kotlinx.coroutines.delay
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
 
-    private val _counter = MutableStateFlow(0)
-    val counter = _counter.asStateFlow()
+    private val _counter = MutableSharedFlow<Int>(replay = 2, extraBufferCapacity = 5)
+    val counter = _counter.asSharedFlow()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,26 +29,29 @@ class MainActivity : AppCompatActivity() {
         }
 
         lifecycleScope.launch {
-            while(true){
-                delay(1000L)
-                _counter.value++
+            for(num in 1..5){
+                delay(500)
+                _counter.emit(num)
             }
         }
 
         lifecycleScope.launch {
-            while(true){
-                counter.collect{
-                    Log.d("@@@First observer", "Счётчик: $it")
-                }
+            counter.collect{
+                Log.d("@@@Observer", "Первый подписчик получил значение: $it")
             }
         }
 
         lifecycleScope.launch {
-            while (true){
-                delay(3000)
-                counter.collect{
-                    Log.d("@@@Second observer", "Счётчик: $it")
-                }
+            delay(1500)
+            counter.collect{
+                Log.d("@@@Observer", "Второй подписчик получил значение: $it")
+            }
+        }
+
+        lifecycleScope.launch {
+            delay(2500)
+            counter.collect{
+                Log.d("@@@Observer", "Третий подписчик получил значение: $it")
             }
         }
     }
